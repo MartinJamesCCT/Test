a6419ed8ff366560e7c62f6ebd0b72a669f00ac1
MartinJamesCCT <sba21165@student.cct.ie>
2021-01-26 21:40:41 +0000

    Week3 Code

---
+++ Week3.py
@@ -0,0 +1,171 @@
+
+#print(5 + 3) # Prints: 8
+
+#sum_value = 5 + 8
+#print(sum_value) # Prints 8
+
+
+
+#sum_2 = 6 + sum_value 
+#print(sum_2) # Prints: 14
+
+#my_list = [1,2,3,4] # Initialize my_list
+
+#my_list_2 = [5,6,7,8] # Initialize my_list_2
+
+#my_list = my_list + my_list_2 # Take the current value of my_list and add my_list_2 to it
+
+#print(my_list) # Prints: [1, 2, 3, 4, 5, 6, 7, 8]
+
+#name = "Hello " # Set name variable to an string 'hello '
+
+#name = name + "World!" # Take the current name value and add 'world' to it
+
+#print(name) # Prints: Hello World!
+
+#my_list = [] # Initialize an empty list
+
+#name = "John" # Initialize name variable to John
+
+#my_list = my_list + name # Adding the current my_list variable to the string name
+
+#print(my_list) # Prints: ['j','o','h','n']
+
+#print(5 - 3) # Prints: 2
+
+#value = 5 - 8
+#print(value) # Prints -3
+
+#value_2 = value - 6 
+#print(value_2) # Prints: -9
+
+#my_list = [1,2,3,4] # Initialize my_list
+
+#my_list = "Santa " * 3 # Take my_list and multiply it by 3
+
+#print(my_list) # Prints: [1,2,3,4,1,2,3,4,1,2,3,4]
+
+#print(5 / 2) # Prints: 2.5
+
+#value = 5 / 3
+#print(value) # Prints 1.6666666666666667
+
+#value_2 = value / 2 
+#print(value_2) # Prints: 0.8333333333333334
+
+#print(5 // 3) # Prints: 1
+
+#value = 5 // 8
+#print(value) # Prints 0 (Remember it ALWAYS rounds down)
+
+#value_2 = value // 6 
+
+#print(value_2) # Prints: 0
+
+#variable_1 = 5 # Initialize the variable to 5
+
+#variable_1 = variable_1 + 5 # Take the current value of the variable and add 5
+
+#variable_1 = variable_1 - 5 # Take the current value of the variable and subtract 5
+
+#variable_1 = variable_1 / 2 # Take the current value of the variable and floating point divide by 2
+
+#variable_1 = variable_1 // 2 # Take the current value of the variable and integer divide by 2
+
+#print (variable_1)
+
+#print(5 > 3) # Prints: True; since 5 is greater than 3
+
+#result = 5 > 3 # You can store the result in a variable
+
+#print(result) # Prints: True
+
+#print(3 > 5) # Prints: False; since 3 is NOT greater than 5
+
+#print(5 > 5) # Prints: False; since 5 is NOT greater than 5 (they are equal)
+
+#print(5 >= 3) # Prints: True; since 5 is greater than 3
+
+#result = 5 >= 3 # You can store the result in a variable
+
+#print(result) # Prints: True
+
+#print(3 >= 5) # Prints: False; since 3 is NOT greater than 5
+
+#print(5 >= 5) # Prints: True; since 5 is equal to 5
+
+#print(5 < 3 and 8 > 6) # Prints: True; since both statements evaluate to True
+
+#print(5 > 3 and 8 > 6) # Prints: False; since the first statement evaluates to False
+
+#print(5 < 3 or 8 > 6) # Prints: True; since both statements evaluate to True
+
+#print(5 > 3 or 8 > 6) # Prints: True; since the second statement evaluates to True
+
+#name = input("Enter name: ") # Take someones name from the command line
+
+#print("john" in name) # Prints: True if john is name given, or False otherwise
+
+#Condiitons
+
+#if condition:
+    # Do stuff at this indentation level (impotant to keep tab or 4 spaces to run code)
+
+
+#x = 2 # Setting up the x variable
+
+#if x < 3:
+#    x += 2 # This will run if x < 3, otherwise it will be skipped over
+
+#print(x) # Since this is on a lower indentation level, this code will run regardless
+
+#if x < 5:
+#    x += 2 # This will run if x < 3, otherwise it will be skipped over
+#    print ("This is printed only if condition is met")
+
+#print(x) # Since this is on a lower indentation level, this code will run regardless
+
+#x = 2 # Setting up the x variable
+
+#if x < 3:
+#    x += 2 # This will run if x < 3, otherwise it will be skipped over
+#else:
+#    x -= 1 # This will run if x is not less than 3
+
+#print(x) # Since this is on a lower indentation level, this code will run regardless
+
+#if x < 3:
+#    x += 2 # This will run if x < 3, otherwise it will be skipped over
+#else:
+#    x -= 1 # This will run if x is not less than 3
+
+#print(x) # Since this is on a lower indentation level, this code will run regardless
+
+#if condition: # You MUST have an initial if statement to use an elif statement
+    # Do stuff
+
+#elif condition_2:
+    # Do other stuff
+
+#else: # This is optional, but useful if you want a catch-all condition
+    # Do other other stuff
+
+    #So...
+
+#user_value = int(input("Enter a number between 0 and 5: "))
+
+#if user_value == 0:
+#    print("Zero")
+#elif user_value == 1:
+#    print("One")
+#elif user_value == 2:
+#    print("Two")
+#elif user_value == 3:
+#    print("Three")
+#elif user_value == 4:
+#    print("Four")
+#elif user_value == 5:
+#    print("Five")
+#else: # If value is not between 0 and 5
+#    print("Value provided is not between 0 and 5!")
+
